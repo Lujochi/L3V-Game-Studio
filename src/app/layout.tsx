5251fa916c7dce4f161af5ca9a499dda69ec277f
Luan Chiodini <a.luanjc@gmail.com>
2025-07-27 10:07:57 -0300

    Alterações: Configurações iniciais do projeto e adicionado cores e imagens.

---
+++ src/app/layout.tsx
@@ -0,0 +1,31 @@
+import "./globals.css";
+import type { ReactNode } from "react";
+
+export const metadata = {
+  title: "LΞV Game Studio",
+  description: "Desenvolvimento de jogos e aplicativos",
+};
+
+export default function RootLayout({ children }: { children: ReactNode }) {
+  return (
+    <html lang="pt-BR">
+      <head>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossOrigin="anonymous"
+        />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Luckiest+Guy&family=Poppins:ital,wght@0,400;0,500;0,700;1,400;1,500;1,700&family=Raleway:ital,wght@0,100..900;1,100..900&family=Roboto:ital,wght@0,100..900;1,100..900&display=swap"
+          rel="stylesheet"
+        />
+        <title>{metadata.title}</title>
+        <meta name="description" content={metadata.description} />
+      </head>
+      <body>{children}</body>
+    </html>
+  );
+}
