38e561ae720423a7ca4601a3f241ed161505331b
Luan Chiodini <a.luanjc@gmail.com>
2025-07-31 12:15:18 -0300

    Alterações: Criado section para equipe e contato.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,9 @@
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <div>
+    <main>
       <header className="flex items-center justify-between pr-32 pl-32 relative h-24 z-1">
         <nav className="flex-1">
           <ul className="flex gap-20 font-bold text-[18px] text-white uppercase">
@@ -13,19 +14,21 @@ export default function Home() {
               { href: "#Contato", label: "Contato" },
             ].map(({ href, label }) => (
               <li key={href}>
-                <a href={href}>{label}</a>
+                <Link href={href}>{label}</Link>
               </li>
             ))}
           </ul>
         </nav>
         <div className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2">
-          <Image
-            src="/Images/logo2.png"
-            alt="LΞV Game Studio"
-            width={176}
-            height={176}
-            className="w-44"
-          />
+          <Link href="/">
+            <Image
+              src="/Images/logo2.png"
+              alt="LΞV Game Studio"
+              width={176}
+              height={176}
+              className="w-44"
+            />
+          </Link>
         </div>
         <div className="flex-1 flex justify-end font-bold text-[18px] text-white uppercase">
           <span className="text-[var(--color-primary)]">PT</span>/
@@ -141,6 +144,124 @@ export default function Home() {
           </div>
         </div>
       </section>
-    </div>
+      <section
+        id="Equipe"
+        className="flex flex-col items-center justify-center bg-black pb-[120px]"
+      >
+        <div className="flex items-center w-full px-4 max-w-4xl mt-16">
+          <div className="flex-grow h-[2px] bg-[var(--color-secondary)]" />
+          <h2 className="mx-8 text-[64px] font-bold text-white uppercase">
+            EQUIPE
+          </h2>
+          <div className="flex-grow h-[2px] bg-[var(--color-secondary)]" />
+        </div>
+        <div className="flex flex-wrap justify-center gap-20 mt-16">
+          <div className="bg-[var(--color-accent)] p-4 rounded-[20px] relative w-[250px] h-[300px] mt-16 border-2 border-[var(--color-secondary)]">
+            <Image
+              src="/Images/img-vitor.png"
+              alt="Imagem equipe"
+              width={150}
+              height={150}
+              className="w-[150px] h-[150px] border-2 border-[var(--color-secondary)] rounded-full absolute top-[-75px] left-1/2 transform -translate-x-1/2"
+            />
+            <div className="flex flex-col items-center text-black mt-18">
+              <h4 className="text-[20px] font-bold">Vitor Henrique</h4>
+              <span className="text-[16px] font-medium">20 Anos</span>
+              <div className="mt-2">
+                <h5 className="text-[16px] font-semibold">Classe:</h5>
+                <ul className="list-disc pl-2 text-[14px] ml-4">
+                  <li>Designer Gráfico</li>
+                </ul>
+                <h5 className="text-[16px] font-semibold mt-4">Atributos:</h5>
+                <ul className="list-disc pl-2 text-[14px] ml-4">
+                  <li>Criador de mundos</li>
+                  <li>Social Mídias</li>
+                </ul>
+              </div>
+            </div>
+          </div>
+          <div className="bg-[var(--color-accent)] p-4 rounded-[20px] relative w-[250px] h-[300px] mt-16 border-2 border-[var(--color-secondary)]">
+            <Image
+              src="/Images/img-luan.jpg"
+              alt="Imagem equipe"
+              width={150}
+              height={150}
+              className="w-[150px] h-[150px] object-cover border-2 border-[var(--color-secondary)] rounded-full absolute top-[-75px] left-1/2 transform -translate-x-1/2"
+            />
+            <div className="flex flex-col items-center text-black mt-18">
+              <h4 className="text-[20px] font-bold">Luan Chiodini</h4>
+              <span className="text-[16px] font-medium">21 Anos</span>
+              <div className="mt-2">
+                <h5 className="text-[16px] font-semibold">Classe:</h5>
+                <ul className="list-disc pl-2 text-[14px] ml-4">
+                  <li>Desenvolvedor</li>
+                </ul>
+                <h5 className="text-[16px] font-semibold mt-4">Atributos:</h5>
+                <ul className="list-disc pl-2 text-[14px] ml-4">
+                  <li>Criador de códigos</li>
+                  <li>Modelagem 3D</li>
+                </ul>
+              </div>
+            </div>
+          </div>
+          <div className="bg-[var(--color-accent)] p-4 rounded-[20px] relative w-[250px] h-[300px] mt-16 border-2 border-[var(--color-secondary)]">
+            <Image
+              src="/Images/img-luciano.png"
+              alt="Imagem equipe"
+              width={150}
+              height={150}
+              className="w-[150px] h-[150px] object-cover border-2 border-[var(--color-secondary)] rounded-full absolute top-[-75px] left-1/2 transform -translate-x-1/2"
+            />
+            <div className="flex flex-col items-center text-black mt-18">
+              <h4 className="text-[20px] font-bold">Luciano Chiodini</h4>
+              <span className="text-[16px] font-medium">19 Anos</span>
+              <div className="mt-2">
+                <h5 className="text-[16px] font-semibold">Classe:</h5>
+                <ul className="list-disc pl-2 text-[14px] ml-4">
+                  <li>Desenvolvedor</li>
+                </ul>
+                <h5 className="text-[16px] font-semibold mt-4">Atributos:</h5>
+                <ul className="list-disc pl-2 text-[14px] ml-4">
+                  <li>Criador de códigos</li>
+                  <li>Animação</li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+      <section
+        id="Contato"
+        className="flex flex-col items-center justify-center bg-white pb-[120px]"
+      >
+        <h2 className="text-[64px] font-bold uppercase mt-16">Contato</h2>
+        <form className="flex items-center justify-center mt-16 w-[1000px] bg-[var(--color-accent)] rounded-[10px] shadow-[0_0_35px_10px_rgba(0,0,0,0.35)]">
+          <Image
+            src="/Images/bg-contato.png"
+            alt="Imagem contato"
+            width={500}
+            height={650}
+          />
+          <div className="flex flex-col items-center justify-center w-[500px]">
+            <h3 className="text-[36px] font-bold mb-[100px]">Fale conosco</h3>
+            <p className="text-[20px] font-medium mb-1">E-mail:</p>
+            <p className="text-[20px] font-bold mb-[50px]">
+              contato@l3vstudio.com.br
+            </p>
+            <span className="text-[20px] font-medium mb-[50px]">Ou</span>
+            <p className="w-[350px] text-center text-[20px] font-medium text-black mb-8">
+              Tem uma ideia de jogo? Vamos conversar e transformar em realidade!
+            </p>
+            <a
+              href="https://l3vstudio.com.br/form/contato"
+              target="_blank"
+              className="bg-[var(--color-secondary)] text-black font-bold py-4 px-8 rounded-lg mt-4 cursor-pointer hover:bg-[var(--color-primary)] transition-colors duration-300 shadow-[4px_4px_10px_-1px_rgba(0,0,0,0.7)]"
+            >
+              Enviar Mensagem
+            </a>
+          </div>
+        </form>
+      </section>
+    </main>
   );
 }
